628b7beea6e7509bebdd66792e92fe31c24801a7
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-04-17 12:51:46 +0300

    feat: add tasks recursive-depth, file-name, get-email-domain

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let renamedNames = {}
+  return names.map((name) => {
+    if (renamedNames.hasOwnProperty(name)) {
+      renamedNames[name]++
+      let newName = `${name}(${renamedNames[name]})`
+      renamedNames[newName] = 0
+      return newName
+    } else {
+      renamedNames[name] = 0
+      return name
+    }
+  })
 }
 
 module.exports = {
-  renameFiles
-};
+  renameFiles,
+}
