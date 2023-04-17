dfa77015351891cb225839dc7b9ccebd4de1fff2
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-04-17 15:57:58 +0300

    feat: add tasks extended-repeater, sort-by-height,transform-array

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|',
+  } = options
+  str = str+'';
+   const additionStr = new Array(additionRepeatTimes)
+     .fill(String(addition))
+     .join(additionSeparator)
+   const result = new Array(repeatTimes).fill(str + additionStr).join(separator)
+   return result
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const heights = arr.filter((el) => el !== -1).sort((a, b) => a - b)
+  const result = []
+    for (const el of arr) {
+      if (el === -1) {
+        result.push(el)
+      } else {
+        result.push(heights.shift())
+      }
+    }
+  return result
 }
 
 module.exports = {
-  sortByHeight
-};
+  sortByHeight,
+}
